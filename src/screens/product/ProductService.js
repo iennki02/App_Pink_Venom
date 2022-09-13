4a7eb2c572ab8959bfaf34981042e1521970fb0c
iennki02 <nhinguyen20022@gmail.com>
2022-09-13 17:24:03 +0700

    new

---
+++ src/screens/product/ProductService.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const ProductService = () => {
+  return (
+    <View>
+      <Text>ProductService</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
---
+++ src/screens/product/screens/EditProfile.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const EditProfile = () => {
+  return (
+    <View>
+      <Text>EditProfile</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
